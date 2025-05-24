c884fb1a27c425a29d1f805a9e9f1e24c73e4149
Anton Silenin <anton-silenin@yandex.ru>
2025-05-24 12:52:37 +0300

    + Table View @ SB + datasource and delegate @ VS

--- ImageFeed/ImagesListViewController.swift.swift
+++ ImageFeed/ImagesListViewController.swift.swift
@@ -11,3 +11,20 @@ class ImagesListViewController: UIViewController {
 
 }
 
+extension ImagesListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension ImagesListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) { }
+    
+}
+
