b03b20c1beb16701ab259e7063b97f774caf4005
Anton Silenin <anton-silenin@yandex.ru>
2025-06-10 15:32:40 +0300

    add ScrollView on SingleImageView

--- ImageFeed/SingleImageView/SingleImageViewController.swift
+++ ImageFeed/SingleImageView/SingleImageViewController.swift
@@ -2,6 +2,13 @@ import UIKit
 final class SingleImageViewController: UIViewController {
     var image: UIImage?
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    @IBAction func didTapBackButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
     @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
