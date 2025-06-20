498a8d9101e89542eb0590e316424f1b599174cd
Anton Silenin <anton-silenin@yandex.ru>
2025-06-20 06:28:42 +0300

    +Auth VC

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Constants {
+    static let accessKey = "gbuNkFdzTCHiGRBSYpNaCZgSA02hkVDj9EQ7iRyumG4"
+    static let secretKey = "gNpFYb9Etl0-2MIQ0SwuV_WntVzlgE4jCYDyzCOqk2w"
+    static let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+}
