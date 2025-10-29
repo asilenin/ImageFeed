74ae002f0ece5fc79c40464d8df5172bdbbb6f6f
Anton Silenin <anton-silenin@yandex.ru>
2025-10-29 20:47:03 +0300

    Unit Tests for WebView

---
+++ ImageFeedTests/Controller/WebViewPresenterSpy.swift
@@ -0,0 +1,36 @@
+import ImageFeed
+import Foundation
+
+final class WebViewPresenterSpy: WebViewPresenterProtocol {
+    var presenter: ImageFeed.WebViewPresenterProtocol?
+    
+    var viewDidLoadCalled: Bool = false
+    
+    var loadRequestCalled = false
+    
+    var view: WebViewViewControllerProtocol?
+    
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+    
+    }
+    
+    func code(from url: URL) -> String? {
+        return nil
+    }
+    
+    func load(request: URLRequest) {
+        viewDidLoadCalled = true
+    }
+
+    func setProgressValue(_ newValue: Float) {
+        // не используется в этом тесте
+    }
+
+    func setProgressHidden(_ isHidden: Bool) {
+        // не используется в этом тесте
+    }
+}
