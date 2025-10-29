4b3032bbb48d5da78c55589d9f3e4782f707e8c7
Anton Silenin <anton-silenin@yandex.ru>
2025-10-29 21:09:37 +0300

    prepare for Unit test for Profile and ImageList VCs UI Test for whole app

---
+++ Tests/UnitTests/ImageListViewTests/ImageListViewTests.swift
@@ -0,0 +1,16 @@
+//
+//  ImageListViewTests.swift
+//  ImageListViewTests
+//
+//  Created by Anton Silenin on 29.10.2025.
+//
+
+import Testing
+
+struct ImageListViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
